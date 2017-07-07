adf399ae74542015d8a126a77ccb50fd3e50d970
Muhammad <mchaudh2@uncc.edu>
2017-07-06 20:30:45 -0400

    adding js

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,20 +1,20 @@
-$(“#search-button”).on(“click”, function() {
+$("#search-button").on("click", function() {
 
-     //
-      var queryURL = “https://api.nytimes.com/svc/search/v2/articlesearch.json“;
-      queryURL += ‘?’ + $.param({
-          ‘api-key’: “5f56b3bc35324cd0ae8eaa45ca05f432”
-        });
+      //
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      queryURL += '?' + $.param({
+      'api-key': "5f56b3bc35324cd0ae8eaa45ca05f432"
+    });
       
-     //
+      //
       $.ajax({
         url: queryURL,
-        method: “GET”
+        method: "GET"
       })
 
-     //
+      //
       .done(function(response) {
-          console.log(response);
-        }).fail(function(err) {
-          throw err;
+        console.log(response);
+      }).fail(function(err) {
+      throw err;
     });
